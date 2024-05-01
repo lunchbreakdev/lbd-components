478b0addeaf23495aacdfdf6ba48558b0ab0992c
Morgan Spencer <morgan@spencercreative.co>
2024-05-01 08:41:15 -0400

    Update build scripts

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
-import { execSync } from 'child_process'
-import fs from 'fs'
-import path from 'path'
+const { execSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
 
 const sourcePath = './template'
 
